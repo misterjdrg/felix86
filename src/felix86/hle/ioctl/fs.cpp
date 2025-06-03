d334a8d280f7b099487758d1e5017ff4ed7a079b
Paris Oplopoios <parisoplop@gmail.com>
2025-06-04 01:10:51 +0300

    Add comment (#137)

--- src/felix86/hle/ioctl/fs.cpp
+++ src/felix86/hle/ioctl/fs.cpp
@@ -2,12 +2,13 @@
 #include <linux/fs.h>
 #include <sys/ioctl.h>
 #include "felix86/common/log.hpp"
-#include "felix86/hle/guest_types.hpp"
 #include "felix86/hle/ioctl/common.hpp"
 #include "felix86/hle/ioctl/fs.hpp"
 
 int ioctl32_fs(int fd, u32 cmd, u32 args) {
     switch (_IOC_NR(cmd)) {
+        // This ioctl (and probably others) are defined to take long* as arguments in the fs.h header but in reality
+        // take an int* so we don't need to do marshalling
         SIMPLE_CASE(FS_IOC_GETFLAGS);
     default: {
         ERROR("Unknown FS ioctl cmd: %x", cmd);
--- src/felix86/hle/ioctl/fs.hpp
+++ src/felix86/hle/ioctl/fs.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include "felix86/common/types.hpp"
