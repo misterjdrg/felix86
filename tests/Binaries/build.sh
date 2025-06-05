061d833003513d4426d6173193b4e63be8a19498
Bohdan <misterjdrg@gmail.com>
2025-06-05 23:28:32 +0800

    build: Add 32bit support to `build.sh`
    We need it for 32bit signal testing
    `IA32=1 ./build.sh Signals`

--- tests/Binaries/build.sh
+++ tests/Binaries/build.sh
@@ -1,7 +1,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (cd $DIR
 for file in **/*.c; do
+
+    echo Compiling ${file%.c}.out
     gcc -O3 -I./ -o ${file%.c}.out $file
     strip ${file%.c}.out
+
+    if [[ "$IA32" -eq 1 ]]; then
+        echo Compiling ${file%.c}_m32.out
+        if gcc -m32 -O3 -I./ -o ${file%.c}_m32.out $file; then
+            strip ${file%.c}_m32.out
+        else
+            echo Failed to compile ${file} in 32bit mode
+        fi
+    fi
 done
 )
