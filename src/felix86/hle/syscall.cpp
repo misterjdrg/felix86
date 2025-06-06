1125f80dbaa51425c948cbb68012c582f336513a
Bohdan <misterjdrg@gmail.com>
2025-06-06 12:58:56 +0800

    syscalls: Add copy_file_range

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -248,6 +248,10 @@ Result felix86_syscall_common(felix86_frame* frame, int rv_syscall, u64 arg1, u6
         result = SYSCALL(close_range, arg1, arg2, arg3);
         break;
     }
+    case felix86_riscv64_copy_file_range: {
+        result = SYSCALL(copy_file_range, arg1, arg2, arg3, arg4, arg5, arg6);
+        break;
+    }
     case felix86_riscv64_shutdown: {
         result = SYSCALL(shutdown, arg1, arg2, arg3, arg4, arg5, arg6);
         break;
