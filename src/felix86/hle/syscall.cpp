13c7ad217ebf9318e077a0c434963d03a29bd59a
Paris Oplopoios <parisoplop@gmail.com>
2025-06-07 13:15:52 +0300

    Implement sendfile syscall (#174)

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -473,6 +473,10 @@ Result felix86_syscall_common(felix86_frame* frame, int rv_syscall, u64 arg1, u6
         result = SYSCALL(syncfs, arg1, arg2, arg3, arg4, arg5, arg6);
         break;
     }
+    case felix86_riscv64_sendfile: {
+        result = SYSCALL(sendfile, arg1, arg2, arg3, arg4);
+        break;
+    }
     case felix86_riscv64_sendmmsg: {
         result = SYSCALL(sendmmsg, arg1, arg2, arg3, arg4);
         break;
